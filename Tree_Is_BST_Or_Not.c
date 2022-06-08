7904914b27f6abbd630a97e373276379cb429a9d
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-06-08 07:37:16 +0530

    Add files via upload

---
+++ Tree_Is_BST_Or_Not.c
@@ -0,0 +1,64 @@
+//CHECK IF A GIVEN TREE IS A BST OR NOT.
+/*The trick is to write a utility helper function isBSTUtil(struct node* node, int min, int max) that traverses down the tree 
+keeping track of the narrowing min and max allowed values as it goes, looking at each node only once. 
+The initial values for min and max should be INT_MIN and INT_MAX — they narrow from there. 
+Note: This method is not applicable if there are duplicate elements with value INT_MIN or INT_MAX.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+};
+int isBSTUtil(struct node* node, int min, int max);
+/* Returns true if the given tree is a binary search tree*/
+int isBST(struct node* node)
+{
+return(isBSTUtil(node, INT_MIN, INT_MAX));
+}
+/* Returns true if the given tree is a BST and its
+values are >= min and <= max. */
+int isBSTUtil(struct node* node, int min, int max)
+{
+/* an empty tree is BST */
+if (node==NULL)
+	return 1;	
+/* false if this node violates the min/max constraint */
+if (node->data < min || node->data > max)
+	return 0;
+/* otherwise check the subtrees recursively,
+tightening the min or max constraint */
+return
+	isBSTUtil(node->left, min, node->data-1) && // Allow only distinct values
+	isBSTUtil(node->right, node->data+1, max); // Allow only distinct values
+}
+/* Helper function that allocates a new node with the
+given data and NULL left and right pointers. */
+struct node* newNode(int data)
+{
+struct node* node = (struct node*)malloc(sizeof(struct node));
+node->data = data;
+node->left = NULL;
+node->right = NULL;
+return(node);
+}
+/* Driver program to test above functions*/
+int main()
+{
+struct node *root = newNode(4);
+root->left	 = newNode(2);
+root->right	 = newNode(5);
+root->left->left = newNode(1);
+root->left->right = newNode(3);
+if(isBST(root))
+	printf("Is BST");
+else
+	printf("Not a BST");
+getchar();
+return 0;
+}
